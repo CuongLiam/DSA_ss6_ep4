38c608386740896e6d16d93d0bb9382d0d3ee1fb
Cuong Liam <cuongdeptrai2706@gmail.com>
2025-07-07 21:30:19 +0700

    up

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void hanoi(int n, char from, char to, char temp);
+
+int main(void) {
+    int n = 2;
+
+    hanoi(n, 'A', 'B', 'C');
+
+    return 0;
+}
+
+void hanoi(int n, char from, char temp, char to) {
+    if (n == 0) return;
+
+    hanoi(n - 1, from, to, temp);
+
+    printf("dia %d di chuyen tu %c sang %c\n", n, from, to);
+
+    hanoi(n - 1, temp, from, to);
+}
